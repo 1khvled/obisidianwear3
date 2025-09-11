e4f11172abc8d6a61885dd8a5a736ef3d2261436
khvled <khvled2004@gmail.com>
2025-09-11 02:43:37 +0100

    Clean up test pages and fix watches category - Remove test pages (cache-test, test-api, test-watches) - Fix watches category display in made-to-order page - Add watches to category order and French translation - Add node_modules to .gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "40b7beb1c60035c1"; }
+/******/ 		__webpack_require__.h = function() { return "d0a0c3b11d104db1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
